8ede7e39fa71eb7d87b82b9bf6b8781fb4f6e5ff
Pablo Montes <169383657+itspablomontes@users.noreply.github.com>
2025-02-15 09:17:17 -0300

    add exercises 3.7 and 3.8 - phonebook backend

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require("express");
+const morgan = require("morgan");
+
 const app = express();
+
 app.use(express.json());
+morgan.token("body", (req) => JSON.stringify(req.body));
+app.use(morgan(`:method :url - :response-time ms :body`));
 
 const PORT = 3001;
 app.listen(PORT, () => {
